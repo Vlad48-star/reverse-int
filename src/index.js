09136fa46b1d46c53e20c5376ad4af4e1bb8cb76
Vlad48-star <85586492+Vlad48-star@users.noreply.github.com>
2021-08-18 14:37:45 +0300

    fix:negativ numbers

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 module.exports = function reverse (n) {
     let reversed = 0;
-    while(n) {
+  if(n < 0){
+    n = n * (-1);
+  }
+  while(n) {
         reversed = reversed * 10 + n % 10;
         n = Math.floor(n / 10);
       }
-    return reversed
+  return reversed
 }
+
